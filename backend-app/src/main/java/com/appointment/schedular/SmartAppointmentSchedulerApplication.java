10f29fbe4b3c2360bfad03efb7bc4ec5a31337a9
Rawat <nimisha.rawat@accenture.com>
2025-11-21 19:22:56 +0530

    Add backend (Spring Boot) and frontend (Angular) projects

---
+++ backend-app/src/main/java/com/appointment/schedular/SmartAppointmentSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.appointment.schedular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartAppointmentSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartAppointmentSchedulerApplication.class, args);
+	}
+
+}
---
+++ backend-app/src/test/java/com/appointment/schedular/SmartAppointmentSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appointment.schedular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartAppointmentSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
